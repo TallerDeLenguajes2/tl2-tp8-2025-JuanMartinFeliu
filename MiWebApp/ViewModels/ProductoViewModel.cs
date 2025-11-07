97ff672945715446bc15b7b1f0e0fa427ccd9471
Juan Martin Feliu <jmfeliu04@gmail.com>
2025-11-07 09:49:08 -0300

    Empezando el tp 9

---
+++ MiWebApp/ViewModels/ProductoViewModel.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaVentas.Web.ViewModels
+{
+    public class ProductoViewModel
+    {
+        public int IdProducto { get; set; }
+
+        //Descripcion con validacion de 250 caracteres. Si no tiene Required es opcional.
+        [Display(Name = " Descripcion del Producto")]
+        [StringLength(250, ErrorMessage = "La descripción no puede superar los 250 caracteres.")]
+        public string Descripcion { get; set; }
+
+        //Precio debe ser positivo y requerido
+        [Display(Name = "Precio Unitario")]
+        [Required(ErrorMessage = "El precio debe ser obligatorio")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe tener un valor positivo")]
+        public double Precio { get; set; }
+    }
+}
